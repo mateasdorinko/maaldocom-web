971c84e83702bc9694fe467dde09d04a3a2c6dd2
mateasdorinko <matt@maaldo.com>
2026-03-05 17:19:04 -0600

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,11 +3,10 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = process.env.APP_BASE_URL || 'https://maaldo.com';
 
-  return ['', '/media-albums', '/writings', '/contact', '/lab', '/terms', '/privacy'].map(
-    (path) => ({
-      url: `${baseUrl}${path}`,
-      changeFrequency: 'weekly',
-      priority: path === '' ? 1 : 0.7,
-    }),
-  );
+  //return ['', '/media-albums', '/writings', '/contact', '/lab', '/terms', '/privacy'].map(
+  return [''].map((path) => ({
+    url: `${baseUrl}${path}`,
+    changeFrequency: 'weekly',
+    priority: path === '' ? 1 : 0.7,
+  }));
 }
